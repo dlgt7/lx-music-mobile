58da42848b96839581e6942eb9510d13a7748839
dlgt7 <102397160+dlgt7@users.noreply.github.com>
2026-09-10 23:01:01 +0800

    Add files via upload

--- src/core/player/playInfo.ts
+++ src/core/player/playInfo.ts
@@ -2,6 +2,7 @@ import playerActions from '@/store/player/action'
 import playerState from '@/store/player/state'
 
 import { getListMusicSync } from '@/utils/listManage'
+import { getDownloadList } from '@/core/download'
 import { setProgress } from '@/core/player/progress'
 import { LIST_IDS } from '@/config/constant'
 
@@ -137,6 +138,5 @@ export const setPlayMusicInfo = (listId: string | null, musicInfo: LX.Download.L
 }
 
 export const getList = (listId: string | null): LX.Music.MusicInfo[] | LX.Download.ListItem[] => {
-  // return listId == LIST_ID_DOWNLOAD ? downloadList : getListMusicSync(listId)
-  return listId == LIST_IDS.DOWNLOAD ? [] : getListMusicSync(listId)
+  return listId == LIST_IDS.DOWNLOAD ? getDownloadList() : getListMusicSync(listId)
 }
