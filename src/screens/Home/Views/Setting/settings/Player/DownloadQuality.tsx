86f2ca079cee7897e75c291871bb3b214314e359
dlgt7 <102397160+dlgt7@users.noreply.github.com>
2026-09-13 23:45:29 +0800

    Add files via upload

--- src/screens/Home/Views/Setting/settings/Player/DownloadQuality.tsx
+++ src/screens/Home/Views/Setting/settings/Player/DownloadQuality.tsx
@@ -7,7 +7,6 @@ import CheckBox from '@/components/common/CheckBox'
 import { useSettingValue } from '@/store/setting/hook'
 import { updateSetting } from '@/core/common'
 import { useI18n } from '@/lang'
-import { TRY_QUALITYS_LIST } from '@/core/music/utils'
 
 const useActive = (id: LX.Quality) => {
   const quality = useSettingValue('download.quality')
@@ -21,7 +20,7 @@ const Item = ({ id, name }: { id: LX.Quality, name: string }) => {
 
 export default memo(() => {
   const t = useI18n()
-  const qualityList = useMemo(() => [...TRY_QUALITYS_LIST, '128k'].reverse() as LX.Quality[], [])
+  const qualityList = useMemo(() => ['flac24bit', 'flac', '320k', '128k'] as LX.Quality[], [])
 
   return (
     <SubTitle title={t('setting_download_quality')}>
